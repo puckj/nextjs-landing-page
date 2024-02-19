4f466cdb6d41937bfb002aed69e60bef5ddc5bc5
Pongsapuck_Dev <pongsapuck.j@gmail.com>
2024-02-19 13:55:42 +0700

    Create Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import ResponsiveNav from "./components/Navigation/ResponsiveNav";
+import Footer from "./components/Footer/Footer";
 
 const poppins = Poppins({
   weight: ["400", "500", "600", "700", "800"],
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className={poppins.className}>
         <ResponsiveNav />
         {children}
+        <Footer />
       </body>
     </html>
   );
